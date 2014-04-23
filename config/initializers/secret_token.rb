14c5ac5c09c7aec8f45cab5c4d268cdf8dddc1da
Tony Chung <anthonyuchung@gmail.com>
2014-04-22 23:36:09 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Easycrud::Application.config.secret_key_base = 'c43065b02d608ea4775d7fa8a325ce0a89fbc763dd439479c21ba06d376286d12b8febbbaffa07b53f11e0b767574565d345cc38d8bc17d9575e4d4340200180'
